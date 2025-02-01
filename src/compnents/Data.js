1a638aa18d0a2fd99645509d09d5beff0aa0daae
habibskhan <116502359+habibskhan@users.noreply.github.com>
2025-02-01 23:56:10 +0530

    Update Data.js

--- src/compnents/Data.js
+++ src/compnents/Data.js
@@ -1,102 +1,131 @@
-
+import { images } from "../sources";
 
 export const howtoorderData = [
-    {
-      id: 1,
-      title: "HOTEL",
-      child: [
-        {
-          id: 1,
-          title: "Frozen Solutions",
-        },
-        {
-          id: 2,
-          title: "Baked Frozen Solutions",
-        },
-        {
-          id: 3,
-          title: "Baked Deliveries",
-        },
-      ],
-    },
-    {
-      title: "RESTAURANT / CAFE / CLOUD KITCHEN",
-      child: [
-        {
-          id: 1,
-          title: "Frozen Solutions2",
-        },
-        {
-          id: 2,
-          title: "Baked Frozen Solutions2",
-        },
-      ],
-    },
-    {
-      id: 3,
-      title: "FOOD SERVICE / CANTEEN / CARTERER",
-      child: [
-        {
-          id: 1,
-          title: "Frozen Solutions3",
-        },
-        {
-          id: 2,
-          title: "Baked Frozen Solutions3",
-        },
-        {
-          id: 3,
-          title: "Baked Deliveries3",
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: "RETAILER",
-      child: [
-        {
-          id: 1,
-          title: "Frozen Solutions",
-        },
-        {
-          id: 2,
-          title: "Baked Frozen Solutions",
-        },
-        {
-          id: 3,
-          title: "Baked Deliveries",
-        },
-        {
-          id: 4,
-          title: "Baked Deliveries",
-        },
-      ],
-    },
-    {
-      id: 5,
-      title: "CONSUMER",
-      child: [
-        {
-          id: 1,
-          title: "Frozen Solutions",
-        },
-        {
-          id: 2,
-          title: "Baked Frozen Solutions",
-        },
-        {
-          id: 3,
-          title: "Baked Deliveries",
-        },
-        {
-          id: 4,
-          title: "Baked Deliveries",
-        },
-        {
-          id: 5,
-          title: "Baked Deliveries",
-        },
-      ],
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "HOTEL",
+    child: [
+      {
+        id: 1,
+        title: "Frozen Solutions",
+      },
+      {
+        id: 2,
+        title: "Baked Frozen Solutions",
+      },
+      {
+        id: 3,
+        title: "Baked Deliveries",
+      },
+    ],
+  },
+  {
+    title: "RESTAURANT / CAFE / CLOUD KITCHEN",
+    child: [
+      {
+        id: 1,
+        title: "Frozen Solutions2",
+      },
+      {
+        id: 2,
+        title: "Baked Frozen Solutions2",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "FOOD SERVICE / CANTEEN / CARTERER",
+    child: [
+      {
+        id: 1,
+        title: "Frozen Solutions3",
+      },
+      {
+        id: 2,
+        title: "Baked Frozen Solutions3",
+      },
+      {
+        id: 3,
+        title: "Baked Deliveries3",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "RETAILER",
+    child: [
+      {
+        id: 1,
+        title: "Frozen Solutions",
+      },
+      {
+        id: 2,
+        title: "Baked Frozen Solutions",
+      },
+      {
+        id: 3,
+        title: "Baked Deliveries",
+      },
+      {
+        id: 4,
+        title: "Baked Deliveries",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "CONSUMER",
+    child: [
+      {
+        id: 1,
+        title: "Frozen Solutions",
+      },
+      {
+        id: 2,
+        title: "Baked Frozen Solutions",
+      },
+      {
+        id: 3,
+        title: "Baked Deliveries",
+      },
+      {
+        id: 4,
+        title: "Baked Deliveries",
+      },
+      {
+        id: 5,
+        title: "Baked Deliveries",
+      },
+    ],
+  },
+];
+
+export const scrollData = [
+  {
+    id: 1,
+    subTitle: "Technical Yarns",
+    subDesc: [
+      `At RCIL, innovation drives production. With a steadfast dedication to research and development, we are proud to be a market leader in the manufacture of technical textiles. We nurture and foster lasting relationships with like minded innovators around the globe to engineer a versatile range of technical yarns that serve a multitude of end-uses. From safeguarding professionals in hazardous environments, enhancing comfort in homes and supporting healthcare advancements to driving progress in transportation, clothing and sportswear, our yarns epitomise the convergence of innovation, functionality and purpose.`,
+    ],
+    ctaLink: "/",
+    img: images.technicalYarn.image,
+  },
+  {
+    id: 2,
+    subTitle: "Technical Yarns 2",
+    subDesc: [
+      `At RCIL, innovation drives production. With a steadfast dedication to research and development, we are proud to be a market leader in the manufacture of technical textiles. We nurture and foster lasting relationships with like minded innovators around the globe to engineer a versatile range of technical yarns that serve a multitude of end-uses. From safeguarding professionals in hazardous environments, enhancing comfort in homes and supporting healthcare advancements to driving progress in transportation, clothing and sportswear, our yarns epitomise the convergence of innovation, functionality and purpose.`,
+    ],
+    ctaLink: "/",
+    img: images.technicalYarn2.image,
+  },
+  {
+    id: 3,
+    subTitle: "Technical Yarns 3",
+    subDesc: [
+      `At RCIL, innovation drives production. With a steadfast dedication to research and development, we are proud to be a market leader in the manufacture of technical textiles. We nurture and foster lasting relationships with like minded innovators around the globe to engineer a versatile range of technical yarns that serve a multitude of end-uses. From safeguarding professionals in hazardous environments, enhancing comfort in homes and supporting healthcare advancements to driving progress in transportation, clothing and sportswear, our yarns epitomise the convergence of innovation, functionality and purpose.`,
+    ],
+    ctaLink: "/",
+    img: images.technicalYarn3.image,
+  },
+];
